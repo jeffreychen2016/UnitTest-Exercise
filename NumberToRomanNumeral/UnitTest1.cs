dd0a99b7e2cec35750cb2e81db917fbb455a7227
Jeffrey Chen <jeffreychen2016@gmail.com>
2018-09-28 20:31:15 -0500

    passed [6,"VI"] test

--- NumberToRomanNumeral/UnitTest1.cs
+++ NumberToRomanNumeral/UnitTest1.cs
@@ -12,6 +12,7 @@ namespace NumberToRomanNumeral
         [InlineData(3, "III")]
         [InlineData(4, "IV")]
         [InlineData(5, "V")]
+        [InlineData(6, "VI")]
 
         // [InlineData(5, "V")]
         public void number_To_RomanNumeral(int number, string expectedRomanNumeral)
