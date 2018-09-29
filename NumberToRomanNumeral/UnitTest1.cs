7fbe62bff56af57cb5bc47ccc0326d9e74a875da
Jeffrey Chen <jeffreychen2016@gmail.com>
2018-09-28 21:12:07 -0500

    Passed [1004, "MVI"]

--- NumberToRomanNumeral/UnitTest1.cs
+++ NumberToRomanNumeral/UnitTest1.cs
@@ -32,7 +32,7 @@ namespace NumberToRomanNumeral
         [InlineData(891, "DCCCXCI")]
         [InlineData(1000, "M")]
         [InlineData(1004, "MIV")]
-        //[InlineData(1006, "MVI")]
+        [InlineData(1006, "MVI")]
         //[InlineData(1023, "MXXIII")]
         //[InlineData(2014, "MMXIV")]
         //[InlineData(3999, "MMMCMXCIX")]
